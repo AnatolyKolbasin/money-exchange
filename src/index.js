df33f639e15c3478e49125f794ff920f6108c020
Anatoly Kolbasin <qwnize@outlook.com>
2019-03-08 11:00:01 +0300

    realize function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	var num = currency;
+    var result = {};
+    if(num < 0) {
+    	return result;
+    }
+    if(num > 10000) {
+        result["error"] = "You are rich, my friend! We don't have so much coins for exchange";
+        return result;
+    }
+    var H = Math.trunc(num/50);
+    var Q = Math.trunc((num-H*50)/25);
+    var D = Math.trunc((num - H*50 - Q*25)/10);
+    var N = Math.trunc((num - H*50 - Q*25 - D*10)/5);
+    var P = num - H*50 - Q*25 - D*10 - N*5;
+    
+    if(H != 0) {
+        result["H"] = H;
+    }
+    if(Q != 0) {
+        result["Q"] = Q;
+    }
+    if(D != 0) {
+        result["D"] = D;
+    }
+    if(N != 0) {
+        result["N"] = N;
+    }
+    if(P != 0) {
+        result["P"] = P;
+    }
+    return result;
 }
